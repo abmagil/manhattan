c52ec3c513734dab139b33db4101d437bcc1eb24
Aaron Magil <amagil@camsys.com>
2014-02-05 18:18:40 -0500

    Works well enough.  Handles responsively enough, has reasonable layout.  Needs something to keep it from scrolling vertically.

--- manhattan_testbed/public/javascripts/boroughs.js
+++ manhattan_testbed/public/javascripts/boroughs.js
@@ -1,21 +1,20 @@
 // Caged from http://eyeseast.github.io/visible-data/2013/08/26/responsive-d3/
 var margin = {top: 10, left: 10, bottom: 10, right: 10}
-  , width = parseInt(d3.select('#main').style('width'))
-  , width = width - margin.left - margin.right
-  , mapRatio = .45
+  , width = parseInt(d3.select('#main').style('width'))  - margin.left - margin.right
+    , mapRatio = .75
   , height = width * mapRatio;
 
-  console.log("width: " + width );
-  console.log("height: " + height );
+console.log(width);
+console.log(height);
 
-var canvas = d3.select("body").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("class", "span8");
+var canvas = d3.select("#main").append("svg")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .attr("viewBox", "0 0 " + width  + " " + height);
       
 var projection = d3.geo.mercator()
       .center([-73.94, 40.70])
-      .scale(70000)
+      .scale(width * 80)
       .translate([(width) / 2, (height)/2]);
       
 var path = d3.geo.path().projection(projection);
@@ -32,6 +31,6 @@ d3.json("javascripts/nybb.geojson", function (data) {
             .selectAll(".state")
             .data(data.features)
             .enter().append("path")
-            .attr("class", function(d){ return d.properties.BoroName; })
+            .attr("class", function(d){ return d.properties.BoroName + " borough"; })
             .attr("d", path);
 });
