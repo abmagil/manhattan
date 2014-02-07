8f1df18f2ed032e9e91af0bffd4b89755b2389d8
Aaron Magil <amagil@camsys.com>
2014-02-06 19:04:52 -0500

    holding point.  The flow is wrong, but the logic is okay...

--- manhattan_testbed/bustime.js
+++ manhattan_testbed/bustime.js
@@ -1,20 +1,38 @@
-var config = require('config');
-
+var config = require('./config')('local');
+var request = require('request');
 // Glue code to pull information from BusTime.  Presumably I will need to bolster and modularize.
 
 var bustime = {};
 
-bustime.agencies = ["MTA NYCT", "MTABC"];
-bustime.baseURL = "http://app." + config.env.target_environment + ".obanyc.com";
+bustime.agencies = ["MTA%20NYCT", "MTABC"];
+bustime.baseURL = "http://app."+config.target_environment +".obanyc.com";
 
 // Pull a JSON listing of routes.  Response objects have
 // shortName: 	human-readable route name
 // longName: 	human-readable description of route
 // color: 		Color associated with the route.
 bustime.getRoutes = function() {
+	routes = [];
 	var routeAPI = "/api/where/routes-for-agency/";
-	var callURL = this.baseURL + routeAPI;
-	console.log(callURL);
+	var host = bustime.baseURL;
+	
+	bustime.agencies.forEach(function(agency) {
+		var path = routeAPI + agency +".json?key=" + config.APIKEY;
+
+		request(host + path, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				var fullroutes = JSON.parse(body);
+				var tmp = fullroutes['data']['list'];	//Just the list of routes
+				tmp.forEach( function(entry) {
+					console.log(entry);
+					routes.push({shortName: entry['shortName'],longName: entry['longName'],color: entry['color']});
+				});
+			}
+			 else {
+			  	console.log(error);
+			 }
+		});
+	});
 };
 
 module.exports = bustime;
